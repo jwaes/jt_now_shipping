8ad35f19697e57dfe687283272fe8667da31881a
jwaes <jaco.waes@gmail.com>
2024-03-01 13:20:11 +0000

    b

--- __manifest__.py
+++ __manifest__.py
@@ -14,13 +14,12 @@
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '2.0',
+    'version': '2.1',
 
     # any module necessary for this one to work correctly
     'depends': [
         'base',
         'delivery',
-        # 'website_sale_delivery',
         'jt_product_properties',
     ],
 
